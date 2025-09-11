93a75da28e32e8e11fc7e10b6024b783746f79f0
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-09-11 09:47:35 -0300

    feat: refactor server to use Express framework and add TypeScript configuration
    - Updated package.json to include Express and TypeScript dependencies.
    - Refactored server implementation in server.ts to utilize Express for handling requests.
    - Added tsconfig.json for TypeScript compiler options.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
 import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-export default defineConfig({});
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
+    },
+  },
+]);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import http from 'http';
+import express from 'express';
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
